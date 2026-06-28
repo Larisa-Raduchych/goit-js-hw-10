20d5f6b80a20b5f11d2d43f1f17f5fcc1262827a
Lari Lari <larisaraduchych@gmail.com>
2026-06-28 14:43:33 +0300

    task-1

---
+++ src/js/1-timer.js
@@ -0,0 +1,80 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+let userSelectedDate;
+
+const datetimePicker = document.querySelector('#datetime-picker');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
+const startButton = document.querySelector('[data-start]');
+startButton.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date()) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+      });
+      startButton.disabled = true;
+    } else {
+      userSelectedDate = selectedDates[0];
+      startButton.disabled = false;
+    }
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+startButton.addEventListener('click', () => {
+  startButton.disabled = true;
+  datetimePicker.disabled = true;
+
+  const intervalId = setInterval(() => {
+    const deltaTime = userSelectedDate - new Date();
+
+    if (deltaTime <= 0) {
+      clearInterval(intervalId);
+      datetimePicker.disabled = false;
+      return;
+    }
+
+    const time = convertMs(deltaTime);
+    daysEl.textContent = addLeadingZero(time.days);
+    hoursEl.textContent = addLeadingZero(time.hours);
+    minutesEl.textContent = addLeadingZero(time.minutes);
+    secondsEl.textContent = addLeadingZero(time.seconds);
+  }, 1000);
+});
